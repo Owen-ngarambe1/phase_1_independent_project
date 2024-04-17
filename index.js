fdf3e8921daa46e2fa6cfcb8812bb350efc6b1a2
owen ngarambe <ngarambe.owen@student.moringaschool.com>
2024-04-17 12:45:14 +0300

    1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+/Write your code here!
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER) + 1;
+console.log(random)
+
+let num3 = 44
+let num4 = 20
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(1, 5, 16 ,18, 20)
+console.log(max)
